46209eac78653b418c42324c3a03805e58b6126b
garayevf <faridjg@code.edu.az>
2025-01-02 04:49:19 +0400

    endpoints

--- webmobileproject2/src/App.js
+++ webmobileproject2/src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/recipes");
+      const data = await response.json();
+      setRecipes(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  
+
+
+  const AddItem = async () => {
+    const newRecipe = { 
+      id: "3", 
+      name: "new Recipe" 
+    };
+
+    try {
+      await fetch(`http://localhost:3000/recipes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newRecipe),
+      });
+      fetchRecipes()
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  const DeleteItem = async () => {
+    const recipeId = 3
+    try {
+      await fetch(`http://localhost:3000/recipes/${3}`, {
+        method: "DELETE",
+      });
+      fetchRecipes()
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  const UpdateItem = async () => {
+    const recipeId = 3
+
+    const updatedRecipe = {
+      "id": "3",
+      "name": "Test Data Updated",
+    }
+
+    try {
+      const response = await fetch(`http://localhost:3000/recipes/${recipeId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedRecipe),
+      });
+      const data = await response.json();
+      fetchRecipes()
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button onClick = {AddItem} >Post</button>
+      <button onClick = {UpdateItem} >Update</button>
+      <button onClick = {DeleteItem}>Delete</button>
+      <br/>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
